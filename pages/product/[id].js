468a186db2d2611117109b2d2ad8aba77905c50b
Vriza Wahyu Saputra <vrizawahyu1922@gmail.com>
2021-07-05 11:45:31 +0700

    Update [id].js

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,14 +1,47 @@
 import React from "react";
 import { useRouter } from 'next/router'
 
-export default function Home() {
+const Home = ({url}) => {
+    console.log("url", url);
     const router = useRouter()
     const { id } = router.query
     return (
         <div>
             <h1>Haloo</h1>
-            <a href={"intent://product/"+id+"#Intent;scheme=help;package=com.helpdelivery;end"}>Klik for Android {id}</a>
-            <a href={"help://product/"+id}>Klik for IOS {id}</a>
+            <a href={url}>Klik disini {id}</a>
         </div>
     );
 };
+
+Home.getInitialProps = async ({ req, res, query }) => {
+    console.log("req", query.id);
+    let userAgent;
+    if (req) {
+        // if on the server, get the user-agent from the headers
+        userAgent = req.headers['user-agent'];
+    } else {
+        // if on the client, access the navigator from the window object
+        userAgent = navigator.userAgent;
+    }
+
+    let url;
+    const isIphone = Boolean(userAgent.match(
+        /iPhone|iPad|iPod/i,
+    ));
+
+    const isAndroid = Boolean(userAgent.match(
+        /Android/i,
+    ));
+
+    if (isAndroid) {
+        url = "intent://product/"+query.id+"#Intent;scheme=help;package=com.helpdelivery;end";
+    } else if (isIphone) {
+        url = "help://product/"+query.id;
+    } else {
+        url = null;
+    }
+    
+    return {url}
+}
+
+export default Home;
